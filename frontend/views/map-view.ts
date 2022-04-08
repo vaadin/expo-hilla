7fcd9bd07211186610709b1432c9456ba5f81a78
Marcus Hellberg <marcus@vaadin.com>
2022-04-07 17:16:52 -0700

    map view

--- frontend/views/map-view.ts
+++ frontend/views/map-view.ts
@@ -1,3 +1,25 @@
-import { View } from './view';
+import * as L from 'leaflet';
+import { html } from 'lit';
+import { customElement, query } from 'lit/decorators.js';
+import { View } from '../views/view';
 
-export class MapView extends View {}
+const openStreetMapLayer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const openStreetMapAttribution = `&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors`;
+
+@customElement('map-view')
+export class MapView extends View {
+  connectedCallback() {
+    super.connectedCallback();
+    this.classList.add('block', 'h-full', 'w-full');
+  }
+
+  firstUpdated() {
+    const map = L.map(this);
+    map.setView([38, -100], 4);
+
+    const tileLayer = L.tileLayer(openStreetMapLayer, { attribution: openStreetMapAttribution, maxZoom: 13 });
+    tileLayer.addTo(map);
+
+    L.marker([33.75, -84.38]).addTo(map);
+  }
+}
