dd49db5b856022d0dbf8a3c7c9b2d389a3fcb4cd
Sebastian Kühnau <seb@vaadin.com>
2023-03-21 06:16:09 +0100

    update hilla version

--- frontend/views/map-view.ts
+++ frontend/views/map-view.ts
@@ -14,11 +14,11 @@ export class MapView extends View {
 
   firstUpdated() {
     const map = L.map(this);
-    map.setView([38, -100], 4);
+    map.setView([50.80, 6.88], 14);
 
     const tileLayer = L.tileLayer(openStreetMapLayer, { attribution: openStreetMapAttribution, maxZoom: 13 });
     tileLayer.addTo(map);
 
-    L.marker([36.1716, -115.1391]).addTo(map);
+    L.marker([50.80061514826724, 6.880651017337574]).addTo(map);
   }
 }
